16b152a5ab3a516477c34b684e9ea83f45ac24b0
BernMeja99 <144741137+BernMeja99@users.noreply.github.com>
2023-12-04 19:13:35 -0500

    Dictornary expanded

--- roster.py
+++ roster.py
@@ -2,7 +2,10 @@
 #https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
 roster = [ 'Davis', 'Ryan', 'Bacot' ]
-player = {"Last Name": roster}
+player = {"Last Name": roster,
+          "First Name": ["RJ","Cormac", "Armando"],
+          "Height": [6,6.5,6.11],
+          "Weight": [180,195,240]}
 data = pd.DataFrame(player)
 print(data)
 
