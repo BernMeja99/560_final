5d06c0548202bbb9823ccdbc9191d5c5c641c285
BernMeja99 <144741137+BernMeja99@users.noreply.github.com>
2023-12-04 19:14:51 -0500

    Removed roster variable

--- roster.py
+++ roster.py
@@ -1,8 +1,7 @@
 # Link to unc roster
 #https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
-roster = [ 'Davis', 'Ryan', 'Bacot' ]
-player = {"Last Name": roster,
+player = {"Last Name": [ 'Davis', 'Ryan', 'Bacot' ],
           "First Name": ["RJ","Cormac", "Armando"],
           "Height": [6,6.5,6.11],
           "Weight": [180,195,240]}
