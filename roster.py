cb004f115281195dd0158b66709f090f46a1ff77
BernMeja99 <144741137+BernMeja99@users.noreply.github.com>
2023-12-04 19:36:01 -0500

    BMI calculations of 3 UNC Basketball players

--- roster.py
+++ roster.py
@@ -1,10 +1,17 @@
 # Link to unc roster
 #https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
+#Player Stats
+#Height is in inches
+#Weight is in Pouns
 player = {"Last Name": [ 'Davis', 'Ryan', 'Bacot' ],
           "First Name": ["RJ","Cormac", "Armando"],
-          "Height": [6,6.5,6.11],
+          "Height": [72, 77,83],
           "Weight": [180,195,240]}
+
 data = pd.DataFrame(player)
+
+#BMI = weight in kg / height in meters
+data["BMI"] = (data["Weight"]/2.205)/ ((data["Height"]/39.37)**2)
 print(data)
 
