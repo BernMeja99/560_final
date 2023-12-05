9cdee93628a9fd8e22bb13fd1226cb365cb53027
BernMeja99 <144741137+BernMeja99@users.noreply.github.com>
2023-12-04 19:08:15 -0500

    last name headers

--- roster.py
+++ roster.py
@@ -2,6 +2,7 @@
 #https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
 roster = [ 'Davis', 'Ryan', 'Bacot' ]
+player = {"Last Name": roster}
 data = pd.DataFrame(roster)
 print(data)
 
